8949e433405c2f7567e3c2271265130aa011e0fb
Praesentia-YKM <ykm8864@naver.com>
2024-03-24 13:06:26 +0900

    Add Java OOP example demonstrating inheritance
    - Provide an example Java class hierarchy demonstrating inheritance
    - Explain the concept of inheritance and its implementation in Java
    - Discuss the benefits of using inheritance in object-oriented programming

---
+++ src/oop/extends1/access/ExtendsAccessMain.java
@@ -0,0 +1,11 @@
+package oop.extends1.access;
+
+import oop.extends1.access.child.Child;
+
+public class ExtendsAccessMain {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}
