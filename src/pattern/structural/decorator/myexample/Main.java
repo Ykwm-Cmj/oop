010967008b70a45b5243c1422f15defa097ecffb
minjae <minjagot@gmail.com>
2024-04-24 21:55:23 +0900

    Update decorator ykm recipe

--- src/pattern/structural/decorator/myexample/Main.java
+++ src/pattern/structural/decorator/myexample/Main.java
@@ -7,7 +7,8 @@ public class Main {
 //        작동 순서까지 런타임에 바꿀수는 없나 생각해보다가 만듬
 //        완전히 데코레이터 구조를 따라가진 않았지만 비슷하게 생각해봄
 //        String[] recipe = {"WATER_HOT", "PAWDER", "NOODLE", "WAIT_3_M"};
-        String[] recipe = {"WATER", "PAWDER", "NOODLE", "FIRE","WAIT_3_M"};
+//        String[] recipe = {"WATER", "PAWDER", "NOODLE", "FIRE","WAIT_3_M"};
+        String[] recipe = {"WATER_HOT", "PAWDER", "NOODLE", "EGG","WAIT_3_M"}; // 권모씨 레시피
         RamenRecipe ramenRecipe = new Equipment(EquipmentEnum.valueOf("POT"));
         for (String r : recipe){
             ramenRecipe.next(new Ingredient(IngredientEnum.valueOf(r)));
--- src/pattern/structural/decorator/myexample/deco/recipe/IngredientEnum.java
+++ src/pattern/structural/decorator/myexample/deco/recipe/IngredientEnum.java
@@ -6,6 +6,7 @@ public enum IngredientEnum {
     , PAWDER("분말스프","를 넣음")
     , FIRE("불", "을 올려 끓임")
     , NOODLE("면", "을 넣음")
+    , EGG("계란", "을 넣음")
     , WAIT_3_M("3분", " 기다려");
     final String name, how;
 
